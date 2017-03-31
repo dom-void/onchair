21733a1d76ba7ff22bcc95e16aa7455dd442e3b9
dom void <doe@onet.pl>
2017-04-01 01:27:35 +0200

    mouseover events done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,24 @@ var submenu = document.querySelectorAll('.submenu');
 var picture = document.querySelectorAll('div.picture-in-picturebox');
 var pictureName = document.querySelectorAll('div.picture-in-picturebox>div');
 
-console.log(menu, submenu, picture, pictureName);
+menu[0].addEventListener('mouseover', function() {
+    submenu[0].classList.remove('hide');
+    submenu[0].classList.add('show');
+})
+menu[0].addEventListener('mouseout', function() {
+    submenu[0].classList.remove('show');
+    submenu[0].classList.add('hide');
+})
+
+for (var i = 0; i < picture.length; i++) {
+    picture[i].addEventListener('mouseover', function() {
+        this.firstElementChild.classList.remove('show');
+        this.firstElementChild.classList.add('hide');
+    })
+    picture[i].addEventListener('mouseout', function() {
+        this.firstElementChild.classList.remove('hide');
+        this.firstElementChild.classList.add('show');
+    })   
+}
 
 })
