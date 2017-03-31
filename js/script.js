435bc87ba8091a3dff98e03b7f6afca11eea4d40
dom void <doe@onet.pl>
2017-04-01 00:45:11 +0200

    js & selector queries working

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var menu = document.querySelectorAll('nav>ul>li');
+var submenu = document.querySelectorAll('.submenu');
+var picture = document.querySelectorAll('div.picture-in-picturebox');
+var pictureName = document.querySelectorAll('div.picture-in-picturebox>div');
+
+console.log(menu, submenu, picture, pictureName);
+
+})
